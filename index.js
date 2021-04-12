fe3d234511b0e5e508bae9b2ed3537db9e09f65d
rilith <bludoftenchiefs@protonmail.com>
2021-04-11 20:07:17 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const writeCards = (['Lisa', 'Kaitlin', 'Jan']);
+let eventName = ('surprise');
+
+function cards(writeCards) {
+    for (let i = 0; i < writeCards.length; i++) {
+        console.log(`Thank you ${writeCards[i]} for the wonderful ${eventName} gift`);   
+    }
+    return writeCards; 
+}
+
+cards(writeCards);
+
+let countDown = 10;
+while (countDown > 0 - 1) {
+  console.log(countDown--);
+} 
